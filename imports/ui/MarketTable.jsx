d964076768ef796cdc9997b0b880c60b3489a02e
Guilherme Nunes <gnunes@eckert-caine.com>
2017-07-06 17:30:18 -0300

    poloniex api integration

--- imports/ui/MarketTable.jsx
+++ imports/ui/MarketTable.jsx
@@ -1,36 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Table, { TableBody, TableHeader, TableRow, TableHeaderColumn, TableRowColumn } from 'material-ui/Table';
-import Paper from 'material-ui/Paper';
 
 class MarketTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ticker: [] };
+  }
+
   componentDidMount() {
-    // test
+    this.setTicker();
+  }
+
+  setTicker() {
+    this.props.session.subscribe('ticker', (ev) => {
+      this.setState({ ticker: ev });
+    });
   }
 
   render() {
     return (
-      <Paper>
-        <Table>
-          <TableHeader>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderColumn>Coin</TableHeaderColumn>
+            <TableHeaderColumn>Price</TableHeaderColumn>
+            <TableHeaderColumn>Volume</TableHeaderColumn>
+            <TableHeaderColumn>Change</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {this.state.ticker.length > 0 &&
             <TableRow>
-              <TableHeaderColumn>Coin</TableHeaderColumn>
-              <TableHeaderColumn>Price</TableHeaderColumn>
-              <TableHeaderColumn>Volume</TableHeaderColumn>
-              <TableHeaderColumn>Change</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableRowColumn>LTC</TableRowColumn>
-              <TableRowColumn>0.10160703</TableRowColumn>
-              <TableRowColumn>24571.112</TableRowColumn>
-              <TableRowColumn>-2.66</TableRowColumn>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Paper>
+              <TableRowColumn>{this.state.ticker[0]}</TableRowColumn>
+              <TableRowColumn>{this.state.ticker[1]}</TableRowColumn>
+              <TableRowColumn>{this.state.ticker[2]}</TableRowColumn>
+              <TableRowColumn>{this.state.ticker[3]}</TableRowColumn>
+            </TableRow>}
+        </TableBody>
+      </Table>
     );
   }
 }
+
+MarketTable.propTypes = {
+  session: PropTypes.object.isRequired,
+};
 
 export default MarketTable;
